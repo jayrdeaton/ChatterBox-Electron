22c60596502dcde8dad04262e310278e816662ca
jaydeaton <jayrdeaton@gmail.com>
2019-08-16 00:26:31 -0500

    added logo to header

--- src/containers/AppHeader.js
+++ src/containers/AppHeader.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import { config } from '../refs';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { Logo } from '../components'
+import { config } from '../refs'
 
-import SettingsButton from './SettingsButton';
-import SoundsButton from './SoundsButton';
-import ThemeTypeButton from './ThemeTypeButton';
+import SettingsButton from './SettingsButton'
+import SoundsButton from './SoundsButton'
+import ThemeTypeButton from './ThemeTypeButton'
 
-import { drawer_actions } from '../actions';
-const { openDrawer } = drawer_actions;
+import { drawer_actions } from '../actions'
+const { openDrawer } = drawer_actions
 
 class AppHeader extends Component {
   render() {
-    const { classes } = this.props;
-    const { open } = this.props.drawer;
+    const { classes } = this.props
+    const { open } = this.props.drawer
     return (
       <AppBar
         position='absolute'
         className={classNames(classes.appBar, open && classes.appBarShift)}
       >
         <Toolbar disableGutters={!open} className={classes.toolbar}>
+          <Logo className={classes.logo} />
           <Typography
             component='h1'
             variant='h6'
@@ -37,13 +39,18 @@ class AppHeader extends Component {
           <SettingsButton />
         </Toolbar>
       </AppBar>
-    );
-  };
-};
+    )
+  }
+}
 const styles = theme => ({
   toolbar: {
     paddingLeft: theme.spacing(),
     paddingRight: theme.spacing()
+  },
+  logo: {
+    height: 50,
+    width: 50,
+    marginRight: theme.spacing()
   },
   appBar: {
     position: 'absolute',
@@ -69,13 +76,13 @@ const styles = theme => ({
   title: {
     flexGrow: 1,
   }
-});
+})
 const mapStateToProps = ({ drawer }) => {
-  return { drawer };
-};
+  return { drawer }
+}
 AppHeader.propTypes = {
   classes: PropTypes.object.isRequired
-};
-AppHeader = connect(mapStateToProps, { openDrawer })(AppHeader);
-AppHeader = withStyles(styles)(AppHeader);
-export default AppHeader;
+}
+AppHeader = connect(mapStateToProps, { openDrawer })(AppHeader)
+AppHeader = withStyles(styles)(AppHeader)
+export default AppHeader
